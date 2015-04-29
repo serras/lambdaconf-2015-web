3abbfeaf14c4a0cc03b0923c1d5e1ae0a6f28f1a
Alejandro Serrano <trupill@gmail.com>
2015-04-29 22:15:04 +0200

    Work on first practical

---
+++ p1-routing/src/Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+import Data.Monoid
+import Web.Spock.Safe
+
+main :: IO ()
+main = runSpock 8080 $ spockT id $ do
+  -- /hello/:name
+  get ("hello" <//> var) $ \name ->
+    text ("Hello, " <> name)
+  -- /allow/:age
+  get ("allow" <//> var) $ \(age :: Integer) ->
+    if age < 21
+       then text "You are not allowed"
+       else text "Please, come in"
